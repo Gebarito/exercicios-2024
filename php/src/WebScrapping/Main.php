04beb8e81f305f51f3ee9ea8666cf79848febe4b
unknown <joaopedrogebara@gmail.com>
2024-04-10 17:43:33 -0300

    PHPCS main.php

--- php/src/WebScrapping/Main.php
+++ php/src/WebScrapping/Main.php
@@ -5,14 +5,12 @@ namespace Chuva\Php\WebScrapping;
 /**
  * Runner for the Webscrapping exercice.
  */
-class Main
-{
+class Main{
 
   /**
    * Main runner, instantiates a Scrapper and runs.
    */
-  public static function run(): void
-  {
+  public static function run(): void {
     ini_set('display-errors', 'off');
     error_reporting(0);
     libxml_use_internal_errors(true);
@@ -23,8 +21,8 @@ class Main
     $scrapper = new Scrapper();
     $papers = $scrapper->scrap($dom);
     
-    $TableWriter = new SpreadsheetWriter();
-    $TableWriter->write($papers);
+    $tableWriter = new SpreadsheetWriter();
+    $tableWriter->write($papers);
   }
 
 }
